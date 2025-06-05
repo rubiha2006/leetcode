444982f594370b393a987978099624b8c9bac77c
rubiha2006 <23bda052@kprcas.ac.in>
2025-06-05 10:58:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.46%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-     return {celsius + 273.15, celsius * 1.8 + 32};
+     return {celsius + 273.15, celsius * 1.80 + 32.00};
 
     }
 };
