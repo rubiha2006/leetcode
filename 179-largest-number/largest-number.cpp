43e97e85061fab9b0e269a342d68adf55f1e5633
rubiha2006 <23bda052@kprcas.ac.in>
2025-06-09 20:42:37 +0530

    Time: 4 ms (55.54%) | Memory: 16.4 MB (72.58%) - LeetSync

---
+++ 179-largest-number/largest-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+     sort(nums.begin(), nums.end(), [](int a, int b) {
+            return to_string(a) + to_string(b) > to_string(b) + to_string(a);
+        });
+        if (nums[0] == 0) return "0";
+        string res;
+        for (int num : nums)
+            res += to_string(num);
+        return res;    
+    }
+};
